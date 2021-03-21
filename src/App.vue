a7e1971cf3260261c6ae4183ef1c430dbcf64e4f
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-20 20:25:16 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <DemoWorld />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import DemoWorld from './components/DemoWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    DemoWorld: DemoWorld
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
